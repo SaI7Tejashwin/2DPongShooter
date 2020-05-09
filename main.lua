e7f2df1e9499a48ca95a0071c3558a884df23cb8
SaI7Tejashwin <57253627+SaI7Tejashwin@users.noreply.github.com>
2020-05-09 22:07:04 +0530

    Add files via upload

--- main.lua
+++ main.lua
@@ -6,21 +6,43 @@ function love.load()
   Object = require "classic"
   require "player"
   require "enemy"
-  p = Player(100, window_height - 100, 200, 50, 400)
+  require "bullets"
+  require "spaceship"
+  ---p = Player(100, window_height - 100, 200, 50, 400)
+  sps = Spaceship()
   e1 = Enemy(100, 100, 100, 100)
   e2 = Enemy(window_width-200, 300, 50, -200)
+  listOfBullets = {}
 end
 
+function love.keypressed(key)
+  ---p:keyPressed(key)
+  sps:keyPressed(key)
+end
 
 
 function love.update(dt)
-  p:update(dt)
+  ---p:update(dt)
+  sps:update(dt)
   e1:update(dt)
   e2:update(dt)
+
+  for i,v in ipairs(listOfBullets) do
+    v:update(dt)
+
+    if v.gone then
+      table.remove(listOfBullets, i)
+    end
+  end
 end
 
 function love.draw()
-  p:draw()
+  ---p:draw()
+  sps:draw()
   e1:draw()
   e2:draw()
+
+  for i,v in ipairs(listOfBullets) do
+    v:draw()
+  end
 end
---
+++ spaceship.lua
@@ -0,0 +1,40 @@
+Spaceship = Object:extend()
+
+
+--player is a Image
+function Spaceship:new()
+  self.image = love.graphics.newImage("Player.png")
+  self.width = self.image:getWidth()
+  self.x = 325
+  self.y = 500
+  self.speed = 500
+end
+
+function Spaceship:draw()
+  love.graphics.draw(self.image, self.x, self.y)
+end
+
+function Spaceship:update(dt)
+  if love.keyboard.isDown("left") then
+    self.x = self.x - self.speed * dt
+  elseif love.keyboard.isDown("right") then
+    self.x = self.x + self.speed * dt
+  end
+
+  local window_width = love.graphics.getWidth()
+
+  if self.x < 0 then --if too far to the left
+    --set x to 0
+    self.x = 0
+
+  elseif self.x + self.width > window_width then
+    --set the position to window_width
+    self.x = window_width - self.width
+  end
+end
+
+function Spaceship:keyPressed(key)
+  if key == "space" then
+    table.insert(listOfBullets, Bullets(self.x +(self.width/2), self.y))
+  end
+end
